09663acd1f485f0f89413a165b95296878a2c540
Filip Rurak <filip.rurak@movecloser.pl>
2023-07-24 10:49:17 +0200

    Added NotFound View

--- src/modules/root/views/NotFound.view.tsx
+++ src/modules/root/views/NotFound.view.tsx
@@ -1,9 +1,31 @@
 import React from 'react'
 
+import CustomParagraph from '../../dsl/Paragraph/Paragraph'
+import CustomButton from '../../dsl/Button/Button'
+import CustomLink from '../../dsl/Link/Link'
+import DrawersWrapper from '../../shared/components/DrawersWrapper/DrawersWrapper'
+import Footer from '../../shared/components/Footer/Footer'
+import Navbar from '../../shared/components/Navbar/Navbar'
+
+import { RouteTargetType } from '../contracts/routes.contracts'
+
 const NotFoundView = () => {
   return (
-    <div>
-      Not Found
+    <div className="NotFoundView">
+      <header className="NotFoundView__navigation-wrapper container-fluid">
+        <Navbar />
+      </header>
+
+      <div className="NotFoundView__content container">
+        <CustomParagraph content="Page not found..." classNames={['h2']} />
+        <CustomLink target="/" targetType={ RouteTargetType.Internal } label={ <CustomButton label="Back to homepage" classNames={['mt-5']} /> } />
+      </div>
+
+      <div className="NotFoundView__footer-wrapper container-fluid">
+        <Footer />
+      </div>
+
+      <DrawersWrapper />
     </div>
   )
 }
